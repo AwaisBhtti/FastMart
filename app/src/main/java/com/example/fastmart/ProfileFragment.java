a7341a323c21d1eb59607b30cff3000424413785
awais <awaisakhtar205@gmail.com>
2026-04-13 13:24:27 +0500

    added logout func

--- app/src/main/java/com/example/fastmart/ProfileFragment.java
+++ app/src/main/java/com/example/fastmart/ProfileFragment.java
@@ -1,5 +1,7 @@
 package com.example.fastmart;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -8,11 +10,25 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import com.google.android.material.button.MaterialButton;
+
 public class ProfileFragment extends Fragment {
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_profile, container, false);
+        View view = inflater.inflate(R.layout.fragment_profile, container, false);
+        MaterialButton btnLogout = view.findViewById(R.id.btnLogout);
+        btnLogout.setOnClickListener(v -> {
+            SharedPreferences sp = requireActivity().getSharedPreferences("pref", getContext().MODE_PRIVATE);
+            SharedPreferences.Editor editor = sp.edit();
+            editor.putBoolean("loggedIn", false);
+            editor.commit();
+            Intent intent = new Intent(getActivity(), LoginActivity.class);
+            startActivity(intent);
+            requireActivity().finish();
+        });
+
+        return view;
     }
 }
